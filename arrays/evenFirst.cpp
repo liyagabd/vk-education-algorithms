e4edc1f0ac124f4f644285bc364c8b7f5633d61c
Лия Габдулхакова <17liya06@gmail.com>
2025-10-19 22:20:15 +0300

    ошибка компиляции

--- arrays/evenFirst.cpp
+++ arrays/evenFirst.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
 
-void evenFirst_void(std::vector<int>& arr) {
+void evenFirst(std::vector<int>& arr) {
     int evenIndex = 0;
 
     for (int i = 0; i < arr.size(); ++i) {
